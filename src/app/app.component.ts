4ff2bb25cbd2e032d6f8859a6d47cf3ea70700a5
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-09-19 16:09:15 -0700

    Added all movies button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -11,6 +12,12 @@ export class AppComponent {
   title = 'angular-myflix-client';
 
   constructor(public dialog: MatDialog) {}
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
+    });
+  }
 
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
