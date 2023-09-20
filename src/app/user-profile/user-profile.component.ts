758f322aff59211cf7eab7caa0974a60b26d3767
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2023-09-20 16:15:21 -0700

    Added user profile component skeleton

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {}
+export class UserProfileComponent {
+  @Input() updatedInfo = { Password: '', Email: '' };
+  constructor(
+    //public dialogRef: MatDialogRef<UserChangeInfoFormComponent>,
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar
+  ) {}
+
+  changeInfo(): void {
+    this.fetchApiData.updateUser(this.updatedInfo).subscribe(
+      (result) => {
+        this.snackBar.open('Updated info successfully!', 'OK', {
+          duration: 2000,
+        });
+      },
+      (result) => {
+        this.snackBar.open('Failed to update info!', 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
