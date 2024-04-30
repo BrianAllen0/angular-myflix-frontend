0d4013411f4bc008adacea8c656cac70101b2e92
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2024-04-30 16:27:55 -0700

    +url tracker last and current url getters

--- src/app/url-tracker.service.ts
+++ src/app/url-tracker.service.ts
@@ -10,6 +10,14 @@ export class UrlTrackerService {
 
   constructor() {}
 
+  public getLastUrl(): string {
+    return this.lastUrl;
+  }
+
+  public getCurrentUrl(): string {
+    return this.currentUrl;
+  }
+
   public updateUrl(urlArray: UrlSegment[]): void {
     // we're about to update - the current is assigned to the last and the argument is assigned to the current
     this.lastUrl = this.currentUrl;
