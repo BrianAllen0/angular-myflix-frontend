a60e7296ea42a8c262a1bbd4cc53cdd5cd4da50b
Brian Allen <123843772+BrianAllen0@users.noreply.github.com>
2024-04-30 15:59:19 -0700

    +url tracker lasturl and currenturl

--- src/app/url-tracker.service.ts
+++ src/app/url-tracker.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UrlTrackerService {
+  private lastUrl: string = '';
+  private currentUrl: string = '';
 
-  constructor() { }
+  constructor() {}
+
+  public setLastURL(value: string) {
+    this.lastUrl = value;
+  }
+  public getLastURL() {
+    return this.lastUrl;
+  }
+
+  public setCurrentURL(value: string) {
+    this.currentUrl = value;
+  }
+  public getCurrentURL() {
+    return this.currentUrl;
+  }
 }
